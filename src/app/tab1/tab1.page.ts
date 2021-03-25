db4a1c24d6191ff2a6054989b86911259cb60c74
jorg3lui5 <jorg3lui5@hotmail.com>
2021-03-24 23:07:44 -0500

    Mostrar películas pipe.module y pipe imagen

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,6 +9,10 @@ import { Pelicula } from '../interfaces/interfaces';
 })
 export class Tab1Page implements OnInit{
   peliculasRecientes : Pelicula[] =[]; 
+  slideOpts = {
+    slidesPerView: 1.3,
+    freeMode: true
+  };
   constructor(
     private moviesService: MoviesService
   ) {}
